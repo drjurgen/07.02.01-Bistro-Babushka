d1198d1cafbf1d066d3a45acfd059c747ad835af
Nicolai Jørgensen <nicolaijorgensen@Nicolais-MacBook-Pro-3.local>
2020-02-10 18:13:30 +0100

    billeder + sortering virker

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const endpoint = "https://spreadsheets.google.com/feeds/list/17Dd7DvkPaFamNUdUKlrFgnH6POvBJXac7qyiS6zNRw0/od6/public/values?alt=json";
 let retter = [];
+let filterKnap = document.querySelectorAll(".filter");
+let kategori = "alle";
+
+
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
     hentData();
+
+    filterKnap.forEach(knap => {
+        knap.addEventListener("click", filtrering);
+    });
+
 }
 
 async function hentData() {
@@ -13,22 +22,40 @@ async function hentData() {
     visRetter();
 }
 
+function filtrering() {
+    filterKnap.forEach(knap => {
+        knap.classList.remove("valgt");
+    });
+
+    this.classList.add("valgt");
+
+    kategori = this.dataset.kategori;
+    console.log(kategori);
+
+    visRetter();
+
+}
+
 
 function visRetter() {
     let container = document.querySelector("#container");
     let retTemplate = document.querySelector("template");
     let article = document.querySelector("article");
 
+    container.innerHTML = "";
+
     retter.feed.entry.forEach(ret => {
-        let klon = retTemplate.cloneNode(true).content;
-        klon.querySelector("h2").textContent = ret.gsx$navn.$t;
-        //klon.querySelector("img").src = ret.gsx$billede.$t;
-        klon.querySelector("img").src = "https://nicohj.dk/kea/07_dynamic_content/07_02_01_bistro_babushka/idk2.png"
+        if (kategori == "alle" || ret.gsx$kategori.$t == kategori) {
+            let klon = retTemplate.cloneNode(true).content;
+            klon.querySelector("h2").textContent = ret.gsx$navn.$t;
+            klon.querySelector("img").src = `imgs/small/${ret.gsx$billede.$t}-sm.jpg`;
 
 
-        klon.querySelector("p").textContent += ret.gsx$kort.$t;
-        klon.querySelector("p:last-child").textContent += ret.gsx$pris.$t + ",-";
-        container.appendChild(klon);
+            klon.querySelector("p").textContent += ret.gsx$kort.$t;
+            klon.querySelector("p:last-child").textContent += ret.gsx$pris.$t + ",-";
+            container.appendChild(klon);
+        }
+
     });
 
 }
